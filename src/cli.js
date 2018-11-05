c15572360a4e5e71e3bbd13ee3dace2d41e85565
Adam Bánky <banky.adam@gmail.com>
2018-11-05 13:44:45 +0100

    fix(cli): proper binding

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,6 @@
 const logTransformer = require('./transformer').create();
 const transformStreamFactory = require('./transform-stream-factory');
 
-const transformStream = transformStreamFactory(logTransformer.transform);
+const transformStream = transformStreamFactory(logTransformer.transform.bind(logTransformer));
 
 process.stdin.pipe(transformStream);
